eaae0a8c4d70e552c5eaa52d6e2013a92d545bda
taylor124 <74930599+taylor124@users.noreply.github.com>
2021-02-12 13:37:13 -0500

    All Engineer tests pass

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -1,1 +1,37 @@
 const Engineer = require('../lib/Engineer');
+
+test('Returns the Name of the Engineer', () => {
+    const engineer = new Engineer('Taylor');
+    
+    if (engineer.name === 'Taylor') {
+        console.log(engineer);
+    };
+    expect(engineer.name = 'Taylor');
+});
+
+test('Returns the Id of the Engineer', () => {
+     const engineer = new Engineer('Taylor', '3');
+
+    if (engineer.id = '3'){
+        console.log(engineer.id);
+    };
+    expect(engineer.id = '3');
+});
+
+test('Returns the Email of the Engineer', () => {
+    const engineer = new Engineer('Taylor', '3', 'vanscoytaylor@gmail.com');
+    
+    if (engineer.email = 'vanscoytaylor@gmail.com'){
+        console.log(engineer.email);
+    };
+    expect(engineer.email = 'vanscoytaylor@gmail.com');
+});
+
+test('Returns the Github of the Engineer', () => {
+    const engineer = new Engineer('Taylor', '3', 'vanscoytaylor@gmail.com', 'taylor124');
+    
+    if (engineer.github === 'taylor124') {
+        console.log(engineer.github);
+    };
+    expect(engineer.github = 'taylor124');
+});
